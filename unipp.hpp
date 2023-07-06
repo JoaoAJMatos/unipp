36e742b4927e66eb94646eb51f829bcd0b67206b
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-07-07 00:25:46 +0100

    core: Removes support for custom test suites
    The way custom test suites were being implemented was not viable. I might come back to that later on

--- unipp.hpp
+++ unipp.hpp
@@ -14,6 +14,7 @@
 
 // TODO: Add support for benchmarking
 // TODO: Add different logging levels for failed tests
+// TODO: Maybe add support for custom test suites with custom setup and teardown functions
 
 #ifndef UNIPP_TEST_FRAMEWORK_HPP
 #define UNIPP_TEST_FRAMEWORK_HPP
@@ -33,19 +34,8 @@
 
 /** Macros for creating and running tests */
 #define TEST(name, description, testfunction) unipp::UnitTest(name, description, testfunction)
-
 #define SUITE(name, description, ...) \
       unipp::TestSuite(name, description, unipp::SetupAndTeardownType::NONE, __VA_ARGS__)
-
-#define CUSTOM_SUITE(custom_suite_class, name, description, setup_type, ...) \
-      custom_suite_class(name, description, setup_type, __VA_ARGS__)
-
-#define DEFAULT_CONSTRUCTOR(custom_suite_name) \
-      template<typename... Tests> \
-      custom_suite_name(std::string name, std::string description, \
-            unipp::SetupAndTeardownType setup_type = unipp::SetupAndTeardownType::PER_TEST, Tests... tests) \
-            : unipp::TestSuite(name, description, setup_type, tests...) { }
-
 #define RUN(...) unipp::TestRunner::RunAll(__VA_ARGS__)
 
 
@@ -64,36 +54,10 @@
 #define ASSERT_NOT_NULL(expr, msg) if ((expr) == nullptr) PANIC(msg)
 
 
-/** Helper Macros for code readability */
-#define RUN_SETUP_BEFORE_SUITE_IF_NEEDED() if (setup_type_ == SetupAndTeardownType::PER_SUITE) Setup()
-#define RUN_TEARDOWN_AFTER_SUITE_IF_NEEDED() if (setup_type_ == SetupAndTeardownType::PER_SUITE) Teardown()
-#define RUN_SETUP_BEFORE_TEST_IF_NEEDED() if (setup_type_ == SetupAndTeardownType::PER_TEST) Setup()
-#define RUN_TEARDOWN_AFTER_TEST_IF_NEEDED() if (setup_type_ == SetupAndTeardownType::PER_TEST) Teardown()
-
-
-
 namespace unipp
 {
       /** Type definitions */
       typedef std::function<void()> TestFunction;
-
-
-      /** 
-       * Setup and teardown types.
-       *
-       * If the setup and teardown procedures are expensive, it is possible to
-       * perform them only once before and after the suite. Keep in mind that the
-       * tests in the suite should not depend on each other. Since one test case
-       * may inadvertently change the state of the system, causing the next test
-       * case to fail, leading to unexpected results and making it difficult to
-       * isolate and diagnose issues within individual test cases (check docs).
-       */
-      enum SetupAndTeardownType
-      {
-            NONE,
-            PER_TEST,
-            PER_SUITE
-      };
 
 
       /** 
@@ -170,33 +134,23 @@ namespace unipp
             {
                   std::cout << "[SUITE | " << this->name_ << " | " << this->description_ << "]" << std::endl;
 
-                  RUN_SETUP_BEFORE_SUITE_IF_NEEDED();
                   for (auto test : tests_) {
                         std::cout << "   [+] Running test: " << test.name << std::endl;
                         std::cout << "   [+] Description: " << test.description << std::endl;
 
                         try {
-                              RUN_SETUP_BEFORE_TEST_IF_NEEDED();
                               test.test();
-                              RUN_TEARDOWN_AFTER_TEST_IF_NEEDED();
                               std::cout << "      [√] PASSED!" << std::endl << std::endl;
                         }
                         catch (const std::exception& e) {
                               std::cout << "      [X] FAILED: " << e.what() << std::endl << std::endl;
                         }
                   }
-                  RUN_TEARDOWN_AFTER_SUITE_IF_NEEDED();
             }
-            
-            /** Virtual Setup and Teardown functions */
-            virtual void Setup() {}
-            virtual void Teardown() {}
       private:
             std::string name_;
             std::string description_;
             std::vector<UnitTest> tests_;
-
-            SetupAndTeardownType setup_type_;
       };
 
 
@@ -243,7 +197,7 @@ namespace unipp
              *        This function accepts an infinite number of tests.
              *        Useful for running all the tests in a readable format using the RUN_ALL macro:
              *
-             *        RUN_ALL(
+             *        RUN(
              *              TEST("My Test", "This is a test description", []() { ... }),
              *              TEST("My Test 2", "This is a test description", []() { ... })
              *        );
